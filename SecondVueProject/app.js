6e4ab9a77d0dcfc8201537714b10f0fd50407527
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2023-12-19 20:52:43 +0000

    Start first Vue projects

---
+++ SecondVueProject/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'Finish the course and learn Vue!',
+            vueLink: 'https://www.google.com/'
+        }
+    } 
+
+});
+
+app.mount('#user-goal');
