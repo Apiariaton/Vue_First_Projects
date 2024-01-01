75a78a8239f91c4596bdca6fad571d6ce800d843
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-01 16:27:58 +0000

    Practise Vue basics

--- ThirdVueProject/basics-assignment-1-problem/app.js
+++ ThirdVueProject/basics-assignment-1-problem/app.js
@@ -1,12 +1,19 @@
 const app = Vue.createApp({
-
-
-
-
-
-
-
-
+    data(){
+        return {
+            name: "W",
+            ageCurrent:"24",
+            agePlusFive:"29",
+            imageURL: "https://images.unsplash.com/photo-1703446592359-fefe8472cc79?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            inputValue: "Pikachu" 
+        }
+    },
+    methods: {
+        outputRandomNumberBetween0And1()
+        {
+            return Math.random();
+        },
+    }
 });
 
 app.mount("#assignment");
