b7ffe18d6d4870195ffd82de684378486abe28a0
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-07 14:22:17 +0000

    Practise using v-on event listeners

---
+++ FifthVueProject/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            userInput: "",
+            confirmedInput:"",
+        };
+    },
+    methods: {
+        showAlert(){
+            alert("Button has been clicked..!");
+        },
+        saveInput(event)
+        {
+            this.userInput = event.target.value;
+        },
+        confirmInput(event)
+        {
+            this.confirmedInput = event.target.value;
+        }
+    }
+});
+
+app.mount("#assignment");
