464761373ef6268ec8b949ccbb3595672f704211
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-08 13:13:16 +0000

    Use a computed property to output a property which is determined by existing Vue data properties

--- FourthVueProject/events/app.js
+++ FourthVueProject/events/app.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
       counter: 0,
       name: "",
       confirmedInput: "",
+      lastName: "H"
     };
+  },
+  computed: {
+    fullName() {
+      if (this.name == '')
+      {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
   },
   methods: {
     incrementCounter(){
@@ -17,13 +27,21 @@ const app = Vue.createApp({
       this.counter += value;
     },
     setName(event, lastName){
-      this.name = event.target.value + lastName;
+      this.name = event.target.value;
     },
     submitForm(){
       alert('Submitted');
     },
     confirmInput(){
       this.confirmedInput = this.name;
+    },
+    resetInput()
+    {
+      this.name = "";
+      this.confirmedInput = name;
+    },
+    outputFullName(){
+     
     }
   }
 });
