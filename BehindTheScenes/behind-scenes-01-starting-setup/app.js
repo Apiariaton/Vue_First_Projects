50bd3750a3276f85a99ec03a015f2856102e3f6f
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-20 17:47:52 +0000

    Introduce Vue.js lifecycle hooks to demarcate phases of Vue.js application's staging

--- BehindTheScenes/behind-scenes-01-starting-setup/app.js
+++ BehindTheScenes/behind-scenes-01-starting-setup/app.js
@@ -11,11 +11,40 @@ const app = Vue.createApp({
     },
     setText() {
       this.message = this.$refs.userText.value;
-    },
+    }
   },
+  beforeCreate()
+  {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount(){
+    console.log("beforeMount()");
+  },
+  mounted(){
+    console.log("mounted");
+  },
+  beforeUpdate(){
+    console.log("beforeUpdate");
+  },
+  updated()
+  {
+    console.log("updated()");
+  },
+  beforeUnmount()
+  {
+    console.log('beforeUnmount');
+  },
+  unmounted()
+  {
+    console.log('unmounted()');
+  }
 });
 
 app.mount('#app');
+app.unmount("#app");
 
 
 const secondApp = Vue.createApp({
