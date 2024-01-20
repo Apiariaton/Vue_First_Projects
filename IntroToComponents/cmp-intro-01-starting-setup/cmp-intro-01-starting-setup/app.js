75e3dd1ad67b4dccb825b3e7de1579a4d2a6d0e7
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-20 18:12:43 +0000

    Create a basic Vue.js component, using app.component(<custom-HTML-element-name, object_specification)

--- IntroToComponents/cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
+++ IntroToComponents/cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
@@ -15,17 +15,40 @@ data()
                 phone: '987654321',
                 email: 'julie@localhost.com'
             },
-        ],
-        detailsAreVisible : false
+        ]    
     }
 },
-methods:
-{
+
+});
+
+app.component('friend-contact',{
+    template:  `<li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetailsAreVisible()">{{detailsAreVisible ? 'Hide' : 'Show Details'}}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+  </li>`,
+    data() {
+        return {detailsAreVisible : false,
+            friend: {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '01234567890',
+                    email: 'manuel@localhost.com'
+                }}
+    },
+    methods:
+    {
     toggleDetailsAreVisible()
     {
         this.detailsAreVisible = !this.detailsAreVisible;
     }
-}
+    }
+
+
 });
+
 
 app.mount("#app");
