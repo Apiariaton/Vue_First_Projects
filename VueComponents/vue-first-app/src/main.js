02a531e10ef09d13093201d49a6973a86b495e35
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-01-22 10:06:44 +0000

    Use style tag within Vue component to style component

--- VueComponents/vue-first-app/src/main.js
+++ VueComponents/vue-first-app/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import FriendContact from "./components/FriendContact.vue";
 
+const app = createApp(App);
 
-createApp(App).mount('#app');
+app.component('friend-contact',FriendContact);
+
+app.mount('#app');
