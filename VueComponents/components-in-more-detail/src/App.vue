e82657d7a30e05a2cae1d254b9d0d8a542427101
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-02-04 15:35:16 +0000

    Use v-slot: / # to specify slot names and slotProps to access props provided by a component to its slots

---
+++ VueComponents/components-in-more-detail/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.itemOne }}</h2>
+        <p>{{ slotProps.itemTwo }}</p>
+      </template>  
+    </course-goals>
+    <div>
+    <active-goals></active-goals>
+    <manage-goals></manage-goals>
+  </div>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+    },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
